52fe6dd863b2bc56121f06efc7b82c41f721c009
CR72322 <1209542541@qq.com>
2023-09-20 16:12:40 -0400

    update

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,21 +1,31 @@
-import sys
-sys.path.append('../src')
-from script import load_data, calculate_average_cost_per_year
 import polars as pl
+import matplotlib.pyplot as plt
 
-def test_load_data():
-    df = load_data("../data.csv")
-    assert df.shape == (8, 4)
-    assert df.columns == ["Entity", "Code", "Year", "Cost of a healthy diet"]
+def load_data(file_path):
+    """Load data using polars and return a DataFrame."""
+    return pl.read_csv(file_path)
 
-def test_calculate_average_cost_per_year():
-    data = {
-        "Entity": ["A", "A", "B", "B"],
-        "Code": ["ALB", "ALB", "DZA", "DZA"],
-        "Year": [2017, 2018, 2017, 2018],
-        "Cost of a healthy diet": [3.9, 4.0, 3.7, 3.8]
-    }
-    df = pl.DataFrame(data)
-    result = calculate_average_cost_per_year(df)
-    assert result.shape == (2, 2)
-    assert result.columns == ["Year", "Average Cost"]
+def calculate_average_cost_per_year(df):
+    """Calculate the average cost of a healthy diet per year."""
+    return df.groupby("Year").agg(pl.col("Cost of a healthy diet").mean().alias("Average Cost"))
+
+def plot_average_cost_per_year(df):
+    """Plot the average cost of a healthy diet per year."""
+    years = df["Year"].to_list()
+    avg_cost = df["Average Cost"].to_list()
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(years, avg_cost, marker='o', linestyle='-', color='b')
+    plt.title("Average Cost of a Healthy Diet per Year")
+    plt.xlabel("Year")
+    plt.ylabel("Average Cost")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    data_path = "data.csv"
+    df = load_data(data_path)
+    avg_cost_df = calculate_average_cost_per_year(df)
+    print(avg_cost_df)
+    plot_average_cost_per_year(avg_cost_df)
